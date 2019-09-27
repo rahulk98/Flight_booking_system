80903caf5fff6c12abfac1f8dd187798aed5acaa
rahulk98 <rahul.krishnan27@gmail.com>
2019-09-27 18:56:23 +0530

    updated jdbc java file

--- jdbc/src/bookmyflight/dbConnector.java
+++ jdbc/src/bookmyflight/dbConnector.java
@@ -25,10 +25,10 @@ public class dbConnector {
 		try {
 			Statement statement = connection.createStatement();
 			ArrayList<String> data = new ArrayList<>();
-			ResultSet result = statement.executeQuery("SELECT flight_id, airline_name, arrival_time, departure_time from flights where source = "+source +" and destination = " + destination + "order by price");
+			ResultSet result = statement.executeQuery("SELECT flight_no, airline_name, arrival_time, departure_time from flights where source = "+ "'" +source +"'"+" and destination = " +"'"+ destination +"'"+ "order by price");
 			String temp;
 			while(result.next()) {
-				temp = result.getString("flight_id") + "," + result.getString("airline_name") +"," +  result.getString("arrival_time")+"," + result.getString("departure_time");
+				temp = result.getString("flight_no") + "," + result.getString("airline_name") +"," +  result.getString("arrival_time")+"," + result.getString("departure_time");
 				data.add(temp);
 			}
 			return data;
@@ -43,7 +43,7 @@ public class dbConnector {
 	public static void main(String[] args) {
 		dbConnector db = new dbConnector();
 		db.connect();
-		System.out.println(db.getFlight("kochi", "trivandrum").get(0));
+		System.out.println(db.getFlight("Coimbatore", "Delhi").get(0));
 	}
 }
 
